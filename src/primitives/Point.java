1da0093171fa46cec48fd2433b661bf7139f2d53
udolev <udolev@yatmal.co.il>
2023-03-01 12:34:49 +0200

    Finished Vector class (besides comments).

--- src/primitives/Point.java
+++ src/primitives/Point.java
@@ -19,27 +19,32 @@ public class Point {
     }
 
     public Point add(Point other) {
-        return new Point(this.xyz.add(other.xyz));
+        return new Point(xyz.add(other.xyz));
     }
 
     public double distanceSquared(Point other) {
-        return (this.xyz.d1 - other.xyz.d1) * (this.xyz.d1 - other.xyz.d1) + (this.xyz.d2 - other.xyz.d2) * (this.xyz.d2 - other.xyz.d2) + (this.xyz.d3 - other.xyz.d3) * (this.xyz.d3 - other.xyz.d3);
+        return (xyz.d1 - other.xyz.d1) * (xyz.d1 - other.xyz.d1) + (xyz.d2 - other.xyz.d2) * (xyz.d2 - other.xyz.d2) + (xyz.d3 - other.xyz.d3) * (xyz.d3 - other.xyz.d3);
     }
 
     public double distance(Point other) {
-        return Math.sqrt(this.distanceSquared(other));
+        return Math.sqrt(distanceSquared(other));
     }
 
     @Override
     public boolean equals(Object obj) {
         if (this == obj) return true;
-        if (obj instanceof Point)
-            return xyz.equals(((Point) obj).xyz);
+        if (obj instanceof Point other)
+            return xyz.equals(other.xyz);
         return false;
     }
 
     @Override
+    public int hashCode() {
+        return xyz.hashCode();
+    }
+
+    @Override
     public String toString() {
-        return super.toString();
+        return "" + xyz;
     }
 }
--- src/primitives/Vector.java
+++ src/primitives/Vector.java
@@ -13,4 +13,50 @@ public class Vector extends Point {
         if (vector.equals(Double3.ZERO))
             throw new IllegalArgumentException("zero vector");
     }
+
+    public Vector add(Vector other) {
+        return new Vector(xyz.add(other.xyz));
+    }
+
+    public Vector scale(double rhs) {
+        return new Vector(xyz.scale(rhs));
+    }
+
+    public double dotProduct(Vector other) {
+        return xyz.d1 * other.xyz.d1 + xyz.d2 * other.xyz.d2 + xyz.d3 * other.xyz.d3;
+    }
+
+    public Vector crossProduct(Vector other) {
+        return new Vector(xyz.d2 * other.xyz.d3 - xyz.d3 * other.xyz.d2, xyz.d3 * other.xyz.d1 - xyz.d1 * other.xyz.d3, xyz.d1 * other.xyz.d2 - xyz.d2 * other.xyz.d1);
+    }
+
+    public double lengthSquared() {
+        return this.dotProduct(this);
+    }
+
+    public double length() {
+        return Math.sqrt(lengthSquared());
+    }
+
+    public Vector normalize() {
+        return new Vector(xyz.scale(1 / length()));
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj instanceof Vector other)
+            return super.equals(other);
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return xyz.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return "‐>" + super.toString();
+    }
 }
