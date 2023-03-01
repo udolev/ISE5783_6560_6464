5a1d03a8cbadcacdf81b0c41117543ff65b58cdc
udolev <udolev@yatmal.co.il>
2023-03-01 11:39:03 +0200

    Update to Polygon class.

--- src/primitives/Vector.java
+++ src/primitives/Vector.java
@@ -3,13 +3,14 @@ package primitives;
 public class Vector extends Point {
 
     public Vector(double x, double y, double z) {
-        super(x,y,z);
-        if(x==0 && y==0 && z==0)
+        super(x, y, z);
+        if (x == 0 && y == 0 && z == 0)
             throw new IllegalArgumentException("zero vector");
     }
+
     Vector(Double3 vector) {
         super(vector);
-        if(vector.equals(Double3.ZERO))
+        if (vector.equals(Double3.ZERO))
             throw new IllegalArgumentException("zero vector");
     }
 }
