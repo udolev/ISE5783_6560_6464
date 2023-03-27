b7e20dc563827ac5972f720008de4a9ea6403fbe
Lenovo <natan5811200@gmail.com>
2023-03-27 11:58:15 +0300

    Complete javadoc for tests

--- unittests/primitives/VectorTest.java
+++ unittests/primitives/VectorTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 /**
  * Unit tests for primitives.Vector class
- * @author natan weis
+ * @author Natan Weis
  */
 class VectorTest {
     /**
