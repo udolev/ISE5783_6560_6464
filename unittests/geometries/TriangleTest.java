f8826f6ab0795b9b5818f1941e9af078145663df
udolev <udolev@yatmal.co.il>
2023-03-27 11:20:29 +0300

    Initial tests structure.

---
+++ unittests/geometries/TriangleTest.java
@@ -0,0 +1,12 @@
+package geometries;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TriangleTest {
+    // No need for reimplementation
+    @Test
+    void getNormal() {
+    }
+}
---
+++ unittests/primitives/VectorTest.java
@@ -0,0 +1,43 @@
+package primitives;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class VectorTest {
+
+    // Ask about subtruct test
+
+    @Test
+    void add() {
+    }
+
+    // No need for reimplementation
+    @Test
+    void subtract() {
+    }
+
+    @Test
+    void scale() {
+    }
+
+    @Test
+    void dotProduct() {
+    }
+
+    @Test
+    void crossProduct() {
+    }
+
+    @Test
+    void lengthSquared() {
+    }
+
+    @Test
+    void length() {
+    }
+
+    @Test
+    void normalize() {
+    }
+}
